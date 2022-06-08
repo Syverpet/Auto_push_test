137a57a90131f0bc9725b094748d6f433b2f9fd0
Syver Petersen <syverpet@gmail.com>
2022-06-08 17:42:50 +0200

    comment from python script

--- Sheets_python_edit.py
+++ Sheets_python_edit.py
@@ -56,9 +56,13 @@ from git import Repo
 def git_push():
     try:
         repo = Repo(PATH_OF_GIT_REPO)
+        print('1')
         repo.git.add(update=True)
+        print('2')
         repo.index.commit(COMMIT_MESSAGE)
+        print('3')
         origin = repo.remote(name='origin')
+        print('4')
         origin.push()
     except:
         print('Some error occured while pushing the code')    
